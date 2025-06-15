a59d70ef0e99f6de424aebbf996adf8fdfe24905
ccadevida03 <c.cadevida03@gmail.com>
2025-06-15 10:31:49 +0800

    updated system name

--- basecode.php
+++ basecode.php
@@ -58,7 +58,7 @@ if ($_SESSION["username"] != $superuser) {
                     <div class="d-sm-flex align-items-center justify-content-between mb-4">
                         <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
                         <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
-                                class="fas fa-exclamation fa-sm text-white-50"></i> WELCOME TO Smiles & More
+                                class="fas fa-exclamation fa-sm text-white-50"></i> WELCOME TO SMILES&MORE
                             OFFICIAL SYSTEM</a>
                     </div>
 
