40c4411099c48cbc6f65ecdaaec3511be71d47a8
Eysi27 <lirio.aaron@gmail.com>
2025-06-20 00:57:30 +0800

    Fixed Monthly expenses year data

--- monthlyexpensesummary.php
+++ monthlyexpensesummary.php
@@ -68,16 +68,16 @@
                                 <select id="year" name="group" onchange="getclientdata();">
 
                                     <option value="2025">2025</option>
-                                    <option value="2025">2026</option>
-                                    <option value="2025">2027</option>
-                                    <option value="2025">2028</option>
-                                    <option value="2025">2029</option>
-                                    <option value="2025">2030</option>
-                                    <option value="2025">2031</option>
-                                    <option value="2025">2032</option>
-                                    <option value="2025">2033</option>
-                                    <option value="2025">2034</option>
-                                    <option value="2025">2035</option>
+                                    <option value="2026">2026</option>
+                                    <option value="2027">2027</option>
+                                    <option value="2028">2028</option>
+                                    <option value="2029">2029</option>
+                                    <option value="2030">2030</option>
+                                    <option value="2031">2031</option>
+                                    <option value="2032">2032</option>
+                                    <option value="2033">2033</option>
+                                    <option value="2034">2034</option>
+                                    <option value="2035">2035</option>
 
 
 
