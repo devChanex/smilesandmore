9ee163b61c62d123239e8e2c818e8d3a0922a7a4
ccadevida03 <c.cadevida03@gmail.com>
2025-07-02 22:54:27 +0800

    remove backup

--- bars/topbar.php
+++ bars/topbar.php
@@ -39,10 +39,7 @@ echo '</strong>
                             <!-- Dropdown - User Information -->
                             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
                                
-                                <a class="dropdown-item" href="#" onclick="backup()">
-                                    <i class="fas fa-download fa-sm fa-fw mr-2 text-gray-400"></i>
-                                    Backup
-                                </a>
+                              
                                 <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
                                     <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
                                     Logout
